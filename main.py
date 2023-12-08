c74ece1ac497ab5456e51b843f5e5f1cfe0aceaa
u810400 <junhyeok.bang@ibki.co.kr>
2023-12-08 16:27:58 +0900

    20231208 탐지일자 추가

--- main.py
+++ main.py
@@ -28,9 +28,12 @@ def excel(filename):
     text_name = ""
     text_src = ""
     text_dst = ""
+    text_date = ""
     p = re.compile("[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+") #ip 정규표현식 적용
+    r = re.compile("[0-9]+\-[0-9]+\-[0-9].+") #날짜 정규표현식
 
     for i in df.index:
+        # 공격유형 로직
         if df['탐지유형'][i] != "":                                  #탐지유형 열이 공백이 아니면 로직 태움
             if text_name != "":                                      #엔터로 탐지유형을 구분하지만 그냥 추가하면 처음에 공백이 들어가므로 변수에 값이 없는 첫 루프면 안넣게 조건 추가
                 text_name = text_name + "\n"
@@ -39,6 +42,7 @@ def excel(filename):
             else:
                 text_name = text_name + df['탐지유형'][i] + "(" + df['탐지 이벤트'][i] + "건)" #문자열에 탐지유형 추가
 
+        # 출발지 로직
         if df['공격대상'][i] != "":                                 #공격대상 열이 공백이 아닌 것만 체크
             if p.match(df['공격대상'][i]):                          #공격대상열에 정규표현식에 해당하는 것만 체크
                 """if text_src != (df['공격대상'][i] + "(" + df.iat[i,5] + "), "): #중복제거 로직 아직 미완
@@ -52,7 +56,7 @@ def excel(filename):
                 if df.iat[i, 7] == "" and text_src !="":            #탐지유형별로 출발지를 구분하기 위해 출발지 문자열이 공백이 아니며(처음공백 피하기 위해) 특정위치가 공백이면 엔터 추가하게 로직 구성
                     text_src = text_src[:-2]                        #마지막 ip는 ", "을 삭제
                     text_src = text_src + "\n"
-
+        #목적지 로직
         if p.match(df.iat[i,7]):
             dup_dst = text_dst.split('(')
             if dup_dst[len(dup_dst)-2].endswith(df.iat[i,7]):
@@ -63,19 +67,36 @@ def excel(filename):
             if df.iat[i,7] == "" and text_dst != "":                            #목적지 문자열이 공백이 아니며 목적지 나열이 끝나면 공백추가
                 text_dst = text_dst[:-2]                                        #마지막 ip는 ", "을 삭제
                 text_dst = text_dst + "\n"
+
+        #탐지시간 로직
+        if r.match(df.iat[i,11]):
+            text_date = text_date + df.iat[i,11][11:-2] + " | "
+        else:
+            if df.iat[i,11] == "" and text_date != "":
+                text_date = text_date[:-3]
+                text_date = text_date + "\n"
+
     text_src = text_src[:-2]
     text_dst = text_dst[:-2]        #출발지 목적지 마직막에 ", " 제거 탐지유형은 하나씩만 있으므로 없어도 됨
+    text_date = text_date[:-3]
+
     list1 = text_name.split("\n")
     list2 = text_src.split("\n")
     list3 = text_dst.split("\n")    #문자열 엔터기준으로 나누기
+    list4 = text_date.split("\n")
 
     excel_report = ""
+    report_date = df.iat[4,11][:11]
+    report_date = report_date.replace("-","/")
     for j in range(0,len(list1)):
-        excel_report = excel_report + "<h1>" +list1[j] + "</h1><h2>" + list2[j] + "</h2><h2>" + list3[j] + "</h2>"
+        list4[j] = report_date + list4[j]#str(datetime.today().strftime("%Y/%m/%d")) + " " + list4[j]
+        excel_report = excel_report + "<h1>" +list1[j] + "</h1><h2>" + list2[j] + "</h2><h2>" + list3[j] + "</h2><h2>" + list4[j] + "</h2>"
     html(excel_report)
+
     print(list1)
     print(list2)
     print(list3)
+    print(list4)
 
 def html(excelfile):
     #html_text = """<!DOCTYPE html><html><head><title>report</title><meta charset="UTF-8"></head><body>""" + excelfile.to_html() + """</body></html>"""
@@ -87,6 +108,6 @@ def html(excelfile):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     #print_hi('PyCharm')
-    a = datetime.today() - timedelta(1)
+    a = datetime.today() - timedelta(2)
     filename = "/Users/aibikeiyeongeumboheom/Desktop/탐지분석_"+a.strftime("%Y-%m-%d")+".xlsx"
     excel(filename)
